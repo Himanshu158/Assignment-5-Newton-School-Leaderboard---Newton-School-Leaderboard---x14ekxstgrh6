f42f2ba5e676500244c18271aa0d393d5155a980
Himanshu <mailboxofhimanshu1999@gmail.com>
2022-01-23 01:16:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,19 @@ const { data } = require('./data')
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-// your code goes here
+
+app.get("/topRankings", async (req, res) => {
+    const off=req.query.offset?req.query.offset:parseInt(0);
+    const lim=req.query.limit?req.query.limit:parseInt(20);
+    const arr=[];
+    
+    console.log(off);
+    console.log(lim);
+    for(let i=off;i<lim;i++){
+        arr.push(data[i]);
+    }
+    res.send(arr);
+});
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
